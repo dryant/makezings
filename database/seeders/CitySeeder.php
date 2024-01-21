56a5f19bc24d1b86bee93a514771d86a384a9031
dryant <dryant@MacBook-Pro-de-dryant.local>
2024-01-21 22:10:09 +0100

    feat: add all Sapin zipcodes seeder

--- database/seeders/CitySeeder.php
+++ database/seeders/CitySeeder.php
@@ -5,6 +5,8 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\City;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\File;
 
 class CitySeeder extends Seeder
 {
@@ -13,13 +15,20 @@ class CitySeeder extends Seeder
      */
     public function run(): void
     {
-        $city = new City();
         
-        $city->postal_code = '28001';
-        $city->name = 'Madrid';
-        $city->region = 'Madrid';
-        $city->country = 'España';
+        $csvFile = File::get('public/data/postalcodes/ES/zipcodes.es.csv');
+        // echo $csvFile;
+        $data = array_map('str_getcsv', explode("\n", $csvFile));
 
-        $city->save();
+        foreach ($data as $row) {
+            DB::table('cities')->insert([
+                'created_at' => now(),
+                'updated_at' => now(),
+                'postal_code' => $row[1],
+                'name' => $row[2],
+                'region' => $row[3],
+                'country' => 'España',
+            ]);
+        }
     }
 }
