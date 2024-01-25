b4e118a9363485713e2f477af0190c6526479a69
dryant <dryant@MacBook-Pro-de-dryant.local>
2024-01-26 00:10:49 +0100

    deleted error branches

--- app/Policies/ZingPolicy.php
+++ app/Policies/ZingPolicy.php
@@ -6,7 +6,7 @@ use App\Models\User;
 use App\Models\Zing;
 use Illuminate\Auth\Access\Response;
 
-class ZingsPolicy
+class ZingPolicy
 {
     /**
      * Determine whether the user can view any models.
