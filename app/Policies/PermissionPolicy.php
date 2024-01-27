c5b9ec37f8ae68825aad9cf79e447c505f802b68
dryant <dryant@MacBook-Pro-de-dryant.local>
2024-01-27 11:32:57 +0100

    add PermissionPolicy

--- app/Policies/PermissionPolicy.php
+++ app/Policies/PermissionPolicy.php
@@ -2,11 +2,11 @@
 
 namespace App\Policies;
 
+use App\Models\Permission;
 use App\Models\User;
-use App\Models\Role;
 use Illuminate\Auth\Access\Response;
 
-class UserPolicy
+class PermissionPolicy
 {
     /**
      * Determine whether the user can view any models.
@@ -19,7 +19,7 @@ class UserPolicy
     /**
      * Determine whether the user can view the model.
      */
-    public function view(User $user, User $model): bool
+    public function view(User $user, Permission $permission): bool
     {
         return $user->hasRole('Admin');
     }
@@ -35,7 +35,7 @@ class UserPolicy
     /**
      * Determine whether the user can update the model.
      */
-    public function update(User $user, User $model): bool
+    public function update(User $user, Permission $permission): bool
     {
         return $user->hasRole('Admin');
     }
@@ -43,7 +43,7 @@ class UserPolicy
     /**
      * Determine whether the user can delete the model.
      */
-    public function delete(User $user, User $model): bool
+    public function delete(User $user, Permission $permission): bool
     {
         return $user->hasRole('Admin');
     }
@@ -51,7 +51,7 @@ class UserPolicy
     /**
      * Determine whether the user can restore the model.
      */
-    public function restore(User $user, User $model): bool
+    public function restore(User $user, Permission $permission): bool
     {
         return $user->hasRole('Admin');
     }
@@ -59,7 +59,7 @@ class UserPolicy
     /**
      * Determine whether the user can permanently delete the model.
      */
-    public function forceDelete(User $user, User $model): bool
+    public function forceDelete(User $user, Permission $permission): bool
     {
         return $user->hasRole('Admin');
     }
