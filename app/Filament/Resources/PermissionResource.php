ed52636f6d24c7f8ca7a8f56aa978fdbed9aabd3
dryant <dryant@MacBook-Pro-de-dryant.local>
2024-01-27 11:57:46 +0100

    fix PermissionPolicy

--- app/Filament/Resources/PermissionResource.php
+++ app/Filament/Resources/PermissionResource.php
@@ -4,40 +4,25 @@ namespace App\Filament\Resources;
 
 use App\Filament\Resources\PermissionResource\Pages;
 use App\Filament\Resources\PermissionResource\RelationManagers;
-use Faker\Provider\ar_EG\Text;
+use App\Models\Permission;
 use Filament\Forms;
 use Filament\Forms\Form;
 use Filament\Resources\Resource;
 use Filament\Tables;
-// use Filament\Tables\TextColumn; // Add this line
+use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
-use Spatie\Permission\Models\Permission;
-use App\Models\User;
-use Filament\Actions\Modal\Actions\Action;
-use Filament\Forms\Components\Select;
-use Filament\Tables\Table;
 use Filament\Forms\Components\TextInput;
-use Filament\Forms\Components\Textarea;
-use Filament\Forms\Components\DateTimePicker;
-use Filament\Tables\Actions\EditAction;
-use Filament\Tables\Actions\DeleteAction;
-use Filament\Tables\Actions\BulkActionGroup;
-use Filament\Tables\Actions\DeleteBulkAction;
-use App\Filament\Resources\StaticAction;
-use Filament\Tables\Actions\Action as ActionsAction;
-use Filament\Tables\Actions;
+use Filament\Forms\Components\Select;
 use Filament\Tables\Columns\TextColumn;
 
 class PermissionResource extends Resource
 {
     protected static ?string $model = Permission::class;
 
-    protected static ?string $navigationIcon = 'heroicon-o-lock-closed';
-    protected static ?int $navigationSort = 30;
-
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
     protected static ?string $navigationGroup = 'Seguridad';
-
+    protected static ?int $navigationSort = 30;
 
     public static function form(Form $form): Form
     {
@@ -58,19 +43,19 @@ class PermissionResource extends Resource
     public static function table(Table $table): Table
     {
         return $table
-            ->columns([
-                TextColumn::make('id'),
-                TextColumn::make('name') 
-                    ->searchable(),
-            ])
-            ->actions([
-                Tables\Actions\EditAction::make(),
-            ])
-            ->bulkActions([
-                Tables\Actions\BulkActionGroup::make([
-                    Tables\Actions\DeleteBulkAction::make(),
-                ]),
-            ]);
+        ->columns([
+            TextColumn::make('id'),
+            TextColumn::make('name') 
+                ->searchable(),
+        ])
+        ->actions([
+            Tables\Actions\EditAction::make(),
+        ])
+        ->bulkActions([
+            Tables\Actions\BulkActionGroup::make([
+                Tables\Actions\DeleteBulkAction::make(),
+            ]),
+        ]);
     }
 
     public static function getRelations(): array
