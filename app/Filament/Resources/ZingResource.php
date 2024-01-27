47e2a18336baeccc5c713c3512ed08ad5a469b65
dryant <dryant@MacBook-Pro-de-dryant.local>
2024-01-27 14:00:34 +0100

    fix filter on zingResource

--- app/Filament/Resources/ZingResource.php
+++ app/Filament/Resources/ZingResource.php
@@ -5,6 +5,7 @@ namespace App\Filament\Resources;
 use App\Filament\Resources\ZingResource\Pages;
 use App\Models\Zing;
 use App\Models\User; // Import the missing User class
+use Filament\Actions\DeleteAction;
 use Filament\Forms;
 use Filament\Forms\Form;
 use Filament\Resources\Resource;
@@ -13,89 +14,115 @@ use Filament\Tables\Columns\ImageColumn;
 use Filament\Tables\Table;
 use Filament\Forms\Components\FileUpload;
 use Spatie\Permission\Traits\HasRoles;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\Select;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Filters\SelectFilter;
+use Illuminate\Database\Eloquent\Builder;
+use Filament\Tables\Actions\EditAction;
+use Filament\Tables\Actions\DeleteBulkAction;
+use Filament\Tables\Actions\BulkActionGroup;
+use Filament\Tables\Actions\Action;
 
 class ZingResource extends Resource
 {
     use HasRoles;
-
+    
     protected static ?string $model = Zing::class;
-
+    
     protected static ?string $navigationIcon = 'heroicon-o-rocket-launch';
-
+    
+    public static function getEloquentQuery(): Builder
+    {
+        return parent::getEloquentQuery()->when(
+            ! auth()->user()->hasAnyRole(['Admin']),
+            fn (Builder $query) => $query->where('user_id', auth()->user()->id)
+        );
+    }
+    
     public static function form(Form $form): Form
     {
         $makers = User::all()->pluck('name', 'id')->toArray();
-
+        
         return $form
-            ->schema([
-                Forms\Components\Select::make('user_id')
-                    ->required()
-                    ->disabled(fn () => auth()->user()->hasRole('Maker') && ! auth()->user()->hasRole('Admin'))
-                    ->default(fn () => auth()->user()->id)
-                    ->relationship('maker', 'name')
-                    ->searchable()
-                    ->required()
-                    ->options($makers)
-                    ->label('Maker'),
-                Forms\Components\TextInput::make('title')
-                    ->required()
-                    ->maxLength(255),
-                Forms\Components\TextInput::make('description')
-                    ->required()
-                    ->maxLength(255),
-                FileUpload::make('image_url')
-                    ->directory('images/zings')
-                    ->image(),
-            ]);
+        ->schema([
+            Select::make('user_id')
+            ->required()
+            ->disabled(fn () => auth()->user()->hasRole('Maker') && ! auth()->user()->hasRole('Admin'))
+            ->default(fn () => auth()->user()->id)
+            ->relationship('maker', 'name')
+            ->required()
+            ->options($makers)
+            ->label('Maker'),
+            TextInput::make('title')
+            ->required()
+            ->maxLength(255),
+            TextInput::make('description')
+            ->required()
+            ->maxLength(255),
+            FileUpload::make('image_url')
+            ->directory('images/zings')
+            ->image(),
+        ]);
     }
-
+    
     public static function table(Table $table): Table
     {
         return $table
-            ->columns([
-                ImageColumn::make('image_url')
-                    ->label('Image'),
-                Tables\Columns\TextColumn::make('maker.name')
-                ->label('Maker'),
-                Tables\Columns\TextColumn::make('title')
-                    ->searchable(),
-                Tables\Columns\TextColumn::make('description')
-                    ->searchable(),
-                Tables\Columns\TextColumn::make('created_at')
-                    ->dateTime()
-                    ->sortable()
-                    ->toggleable(isToggledHiddenByDefault: true),
-                Tables\Columns\TextColumn::make('updated_at')
-                    ->dateTime()
-                    ->sortable()
-                    ->toggleable(isToggledHiddenByDefault: true),
+        ->columns([
+            ImageColumn::make('image_url')
+            ->label('Image'),
+            TextColumn::make('maker.name')
+            ->hidden(fn () => auth()->user()->hasRole('Maker') && ! auth()->user()->hasRole('Admin'))
+            ->searchable()
+            ->label('Maker'),
+            TextColumn::make('title')
+            ->searchable(),
+            TextColumn::make('created_at')
+            ->dateTime()
+            ->sortable()
+            ->toggleable(isToggledHiddenByDefault: true),
+            TextColumn::make('updated_at')
+            ->dateTime()
+            ->sortable()
+            ->toggleable(isToggledHiddenByDefault: true),
             ])
             ->filters([
-                //
-            ])
-            ->actions([
-                Tables\Actions\EditAction::make(),
-            ])
-            ->bulkActions([
-                Tables\Actions\BulkActionGroup::make([
-                    Tables\Actions\DeleteBulkAction::make(),
-                ]),
-            ]);
-    }
-
-    public static function getRelations(): array
-    {
-        return [
-            //
-        ];
-    }
-
-    public static function getPages(): array
-    {
-        return [
-            'index' => Pages\ListZings::route('/'),
-            'create' => Pages\CreateZing::route('/create'),
-            'edit' => Pages\EditZing::route('/{record}/edit'),
-        ];
-    }
-}
+                SelectFilter::make('maker_id')
+                ->relationship('maker', 'name')
+                ->searchable()
+                ->hidden(fn () => auth()->user()->hasRole('Maker') && ! auth()->user()->hasRole('Admin'))
+                ->label('Maker'),
+                ])
+                ->actions([
+                    EditAction::make(),
+                    Action::make('delete')
+                    ->requiresConfirmation()
+                    ->color('danger')
+                    ->icon('heroicon-o-trash')
+                    ->action(fn (Zing $record) => $record->delete())
+                    ])
+                    ->bulkActions([
+                        BulkActionGroup::make([
+                            DeleteBulkAction::make(),
+                        ]),
+                    ]);
+                }
+                
+                public static function getRelations(): array
+                {
+                    return [
+                        //
+                    ];
+                }
+                
+                public static function getPages(): array
+                {
+                    return [
+                        'index' => Pages\ListZings::route('/'),
+                        'create' => Pages\CreateZing::route('/create'),
+                        'edit' => Pages\EditZing::route('/{record}/edit'),
+                    ];
+                }
+            }
+            
